feac53ed9fd147561809b14deeae430f8045b8bf
Vinicius Lopes de Camargo <vinicius_lopes_camarho@hotmail.com>
2015-09-09 11:25:01 -0300

    Atualização e finalização do Ep6 de Filtros com AngularJS

--- js/scriptsEp6.js
+++ js/scriptsEp6.js
@@ -2,12 +2,23 @@
 var mvcApp = angular.module('Filtroapp', []);
 
 mvcApp.controller('ep06Controller', function ep06Controller($scope){
+
+    $scope.data = new Date();
+
+    $scope.letras = "abcdefghijklmnopqrstuvxz"
+
+    $scope.filtro = "";
             
     $scope.states = [
-        {name:'Minas Gerais',capital:'Belo Horizonte', pib:"1000"},
+        {name:'Minas Gerais',capital:'Belo Horizonte', pib:"1300"},
         {name:'São Paulo',capital:'São Paulo', pib:"2000"},
         {name:'Paraíba',capital:'João Pessoa', pib:"500"},
-        {name:'Rio de Janeiro',capital:'Rio de Janeiro', pib:"1500"}
+        {name:'Rio de Janeiro',capital:'Rio de Janeiro', pib:"1500"},
+        {name:'Santa Catarina ',capital:'Florianopolis', pib:"1700"},
+        {name:'Bahia',capital:'Salvador', pib:"1000"},
+        {name:'Rio Grande do Sul',capital:'Porto Alegre', pib:"1600"},
+        {name:'Acre',capital:'Rio Branco', pib:"400"},
+        {name:'Amazônia',capital:'Manaus', pib:"1400"}
     ];
 
     $scope.addState = function(){
